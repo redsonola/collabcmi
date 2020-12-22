746c6b0b50856e019b898253c9b50f300d777960
courtney <redsonola@gmail.com>
2020-12-22 12:45:43 -0600

    add script to cherry pick space between commits

---
+++ scripts/cherry-pick-sb.sh
@@ -0,0 +1,7 @@
+# Usage:
+# get the git commit hash from space between and run ``./scripts/cherry-pick-sb.sh <commit hash>``
+# e.g.:
+# ./scripts/cherry-pick-sb.sh 3fb30af6712905940ab05373f10bccc7c02e9083
+
+git fetch space-between
+git cherry-pick $1
