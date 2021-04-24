6751d85f0db093d71426565e77db611da44d70e3
redsonola@gmail.com <redsonola@gmail.com>
2021-04-23 22:47:31 -0400

    added instructions

---
+++ src/components/Instructions.svelte
@@ -0,0 +1,86 @@
+<svelte:head>
+  <title>Instructions for Skin Hunger</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="/about.css">
+</svelte:head>
+
+<style>
+body {font-family: "verdana", Geneva, sans-serif; }
+h1, h2, h3, h4, h5, h6 {
+  
+  letter-spacing: 5px;
+}
+</style>
+
+<!-- Navbar (sit on top) 
+<div class="w3-top">
+  <div class="w3-bar w3-white w3-padding w3-card" style="letter-spacing:4px;">
+    <a href="#home" class="w3-bar-item w3-button">Gourmet au Catering</a>
+     Right-sided navbar links. Hide them on small screens 
+    <div class="w3-right w3-hide-small">
+      <a href="#about" class="w3-bar-item w3-button">About</a>
+      <a href="#menu" class="w3-bar-item w3-button">Menu</a>
+      <a href="#contact" class="w3-bar-item w3-button">Contact</a>
+    </div>
+  </div>
+</div>
+-->
+
+<!-- Header -->
+<header class="w3-display-container w3-content w3-wide" style="max-width:1600px;min-width:500px" id="home">
+  <img class="w3-image" src="/img/skinhunger.jpg" alt="Skin Hunger" width="1600" height="500">
+  <div class="w3-display-bottomleft w3-padding-large w3-opacity">
+  <p>
+
+  </div>
+</header>
+
+<!-- Page content -->
+<div class="w3-content" style="max-width:1100px">
+
+  <!-- About Section -->
+   <div class="w3-row w3-padding-64" id="about">
+    <!-- <div class="w3-col m6 w3-padding-large w3-hide-small">
+     <img src="/img/aboutimg.jpg" class="w3-round w3-image w3-opacity-min" alt="about Skin Hunger" width="600" height="750"><br>
+     <img src="/img/aboutimg2.jpg" class="w3-round w3-image w3-opacity-min" alt="about Skin Hunger 2" width="600" height="750"><br>
+        <img src="/img/aboutimg3.jpg" class="w3-round w3-image w3-opacity-min" alt="about Skin Hunger 3" width="600" height="750">
+    </div>  -->
+
+    <!-- <div class="w3-col m6 w3-padding-large"> -->
+      <h1 class="w3-center">Instructions</h1><br>
+    <!-- </div> -->
+      <!-- <h5 class="w3-center">Courtney Brown, Melanie Clemmons, Ira Greenberg, Brent Brimhall -->
+<!-- </h5> -->      
+<div style="padding:75% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/540680381?color=ffffff&amp;title=0&amp;byline=0&amp;portrait=0\" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="SKINGHUNGER_SEAMUS"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>  
+    </div>
+  <!-- </div> -->
+  
+  <!-- <hr> -->
+  
+  <!-- Bios -->
+  <!-- <div class="w3-row w3-padding-64" id="menu">
+    <div class="w3-col l6 w3-padding-large">
+      <h1 class="w3-center">Creators</h1><br> 
+      <h4><a href="http://www.courtney-brown.net/" target="_blank" class="w3-hover-text-deep-purple">Courtney Brown</a></h4>
+      <p class="w3-text-grey">Courtney Brown is a composer/performer, software developer, and tango dancer. Her work includes  new musical interfaces, sound installations, and interactive musical performance + improvisation.</p><br>
+    
+      <h4><a href="http://www.instagram.com/auraquartz" target="_blank" class="w3-hover-text-deep-purple">Melanie Clemmons</a></h4>
+      <p class="w3-text-grey">Melanie Clemmons (she/her) is a new media artist interested in reimagining our use of digital technology toward a softer, calmer, and more careful future. She makes videos, net art, installations and XR experiences.</p><br>
+    
+      <h4><a href="http://iragreenberg.org/" target="_blank" class="w3-hover-text-deep-purple">Ira Greenberg</a></h4>
+      <p class="w3-text-grey">With an eclectic background combining studio arts and computer science, Ira Greenberg has been a painter, 2D and 3D animator, print designer, web and interactive designer/developer, programmer, art director, creative director, managing director, art and computer science professor and author.</p><br>
+    
+      <h4><a href="https://www.linkedin.com/in/brent-brimhall-55836010" target="_blank" class="w3-hover-text-deep-purple">Brent Brimhall</a></h4>
+      <p class="w3-text-grey">Brent Brimhall is a dancer and software developer with over ten years of web development experience. He currently works at Motorola, Inc.</p><br>
+        
+    </div>
+    
+
+  </div> -->
+</div>
+
+<!-- Footer -->
+<!-- <footer class="w3-center w3-grey w3-padding-32">
+  <p><a href="/" title="home" class="w3-hover-text-deep-purple">Back to Skin Hunger</a></p>
+</footer> -->
